0d9e570c227ebc8d2114efd742a060cb0dc362ac
최진원갤럭시북3\yun72_92xubzr <munjwc25@gmail.com>
2026-07-12 01:14:32 +0900

    feat(retactic): improve spatial visuals and formations

--- 내가 축구 감독이라면/src/components/UniversalStudio.tsx
+++ 내가 축구 감독이라면/src/components/UniversalStudio.tsx
@@ -5,7 +5,7 @@ import { createGenericSquad, createOpponentSquad } from '../data/generic'
 import { formationPositions, roleOptions } from '../data/match'
 import type { FormationKey, Player, Tactics } from '../types'
 
-const formations: FormationKey[] = ['4-3-3', '4-2-3-1', '3-4-3', '5-3-2']
+const formations: FormationKey[] = ['4-3-3', '4-2-3-1', '4-4-2', '4-1-4-1', '4-3-1-2', '3-4-3', '5-3-2']
 const storageKey = 'retactic-universal-studio-v1'
 
 interface MatchContext {
